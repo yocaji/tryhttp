c3d13d2ca05a897c4d70e03f187b75d6d1c98695
yocaji <yocaji@stone.sakura.ne.jp>
2024-09-01 21:14:04 +0900

    path中のpagesをlettersに変更

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import favicon from 'serve-favicon';
 
 const app = express();
 const port = 3000;
-const APP_NAME = 'TryHTTP';
+const APP_NAME = '山羊郵便';
 
 // Middleware to parse the body of the request
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,19 +23,21 @@ app.get('/', (request: Request, response: Response) => {
   response.render('index', { APP_NAME });
 });
 
-app.get('/pages/new', (request: Request, response: Response) => {
-  response.render('new', { APP_NAME });
+app.get('/letters/new', (request: Request, response: Response) => {
+  const page_title: string = '手紙を出す';
+  response.render('new', { APP_NAME, page_title });
 });
 
-app.post('/pages/create', (request: Request, response: Response) => {
+app.post('/letters/create', (request: Request, response: Response) => {
   const title: string = request.body.title;
   const content: string = request.body.content;
-  response.redirect(`/pages/echo?title=${title}&content=${content}`);
+  response.redirect(`/letters/echo?title=${title}&content=${content}`);
 });
 
-app.get('/pages/echo', (request: Request, response: Response) => {
+app.get('/letters/echo', (request: Request, response: Response) => {
   const { title, content } = request.query;
-  response.render('echo', { APP_NAME, title, content });
+  const page_title: string = '手紙を送りました';
+  response.render('echo', { APP_NAME, page_title, title, content });
 });
 
 // Start the server
